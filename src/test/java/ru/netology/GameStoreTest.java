e5e772d0d10d8029d6da39798ed1be22de500f03
Василий Скоробогатов <vaskorobogatov@yandex.ru>
2022-09-04 20:44:26 +0300

    GameStoreTest added

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,18 +1,63 @@
 package ru.netology;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameStoreTest {
 
-    @Test
-    public void shouldAddGame() {
+  @Test
+  public void shouldAddGame() {
 
-        GameStore store = new GameStore();
-        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+    GameStore store = new GameStore();
+    Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
-        assertTrue(store.containsGame(game));
-    }
+    assertTrue(store.containsGame(game));
+  }
 
-    // другие ваши тесты
+  @Test
+  public void shouldNotFoundGameWithoutErrors() {
+    GameStore store = new GameStore();
+    Game game = null;
+
+    assertFalse(store.containsGame(game));
+  }
+
+  @Test
+  public void shouldAddPlaytimeForExistPlayer() {
+    GameStore store = new GameStore();
+    store.playedTime.put("Vasya", 11);
+    store.addPlayTime("Vasya", 1);
+
+    assertEquals(12, store.playedTime.get("Vasya"));
+  }
+
+  @Test
+  public void shouldAddPlaytimeForNewPlayer() {
+    GameStore store = new GameStore();
+    store.addPlayTime("Petya", 1);
+
+    assertEquals(1, store.playedTime.get("Petya"));
+  }
+
+  @Test
+  public void shouldGetMostActivePlayer() {
+    GameStore store = new GameStore();
+    store.playedTime.put("Vasya", 11);
+    store.playedTime.put("Petya", 10);
+
+    Assertions.assertEquals("Vasya", store.getMostPlayer());
+  }
+
+  @Test
+  public void shouldGetSumOfPlayedTime() {
+    GameStore store = new GameStore();
+    store.playedTime.put("Vasya", 11);
+    store.playedTime.put("Petya", 10);
+
+    Assertions.assertEquals(21, store.getSumPlayedTime());
+  }
+
+  // другие ваши тесты
 }
